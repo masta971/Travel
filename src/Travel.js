912d1904904417b6ac296151fd368711f07faece
masta <ludowild971@gmail.com>
2019-01-29 09:42:08 +0100

    react 2

--- src/Travel.js
+++ src/Travel.js
@@ -1,7 +1,15 @@
-import React from 'react'
-import "./App.css" 
+import React from 'react';
+import "./App.css" ;
+import logo from './logo.jpg';
+
+
+
+
+
+    
 
 var voyage = [
+    
     {
     Destination:'Moule',
     Country:'Gwadada', 
@@ -14,6 +22,25 @@ var voyage = [
     Country:'Réunion', 
     Photo: 'https://cdn.pixabay.com/photo/2014/08/20/00/18/waves-422158_960_720.jpg',
     Distance:'9300km',
+    },
+
+    {
+    Destination:'Kourou',
+    Country:'Guyana', 
+    Photo: 'https://cdn.pixabay.com/photo/2015/03/12/20/30/lighthouse-670821_960_720.jpg ',
+    Distance:'7090km',
+    },
+    {
+    Destination:'Diamant',
+    Country:'Martinique', 
+    Photo: 'https://cdn.pixabay.com/photo/2013/11/07/23/43/martinique-207155_960_720.jpg ',
+    Distance:'6800km',
+    },
+    {
+        Destination:'Nouméa',
+        Country:'Nouvelle-Calédonie', 
+        Photo: 'https://cdn.pixabay.com/photo/2018/10/30/02/49/sunset-3782606_960_720.jpg ',
+        Distance:'16900km',
     },
 ]
 
